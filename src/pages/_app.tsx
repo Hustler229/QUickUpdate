989282f7ebc63494aec70c1beb080ae2227d68bd
Igor Adande <igoradande44@gmail.com>
2024-11-24 05:24:39 +0100

    google verification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>QuickUpdate</title>
         <meta name="description" content="Mise à jour et gestion des contacts avec QuickUpdate" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="t6-xPH_Atw8j9xNjhXJsvMfLcLI3k5fu0z2NarSe6Cg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GoogleOAuthProvider clientId={"1081148467028-h8p6o9urd1rh8g0kf8h76mjba86rdajp.apps.googleusercontent.com"}>
